7b9995bcd0ac3f2fb19750106791f3174b65978a
Aditya Singh <adips@amazon.com>
2022-01-26 23:37:59 +0530

    added merged sorted list

---
+++ practice/mergedSortedArrays.py
@@ -0,0 +1,34 @@
+def mergeSortedArrays(arr1, arr2):
+  merged_arr_1=[]
+  # Way 1
+  for i in range(len(arr1)):
+    for j in range(len(arr2)):
+      if arr1[i] < arr2[j]:
+        merged_arr_1.append(arr1[i])
+        # arr1.remove(arr1[i])
+        break
+      else:
+        merged_arr_1.append(arr2[j])
+        # arr2.remove(arr2[j])  
+  # Way 2
+  # Works with same length arrays
+  #  Iterate simantaneously and compare elems from both arrays whichever is less goes first. Also if an item already exists skip that.
+  merged_arr_2=[]
+  for i,j in zip(arr1, arr2):
+    # print(i,j)
+    if i in merged_arr_2:
+      merged_arr_2.append(j)
+    elif j in merged_arr_2:
+      merged_arr_2.append(i)       
+    elif i == j:
+      merged_arr_2.append(i)    
+    elif i > j: 
+      merged_arr_2.extend([j,i])
+    else:
+      merged_arr_2.extend([i,j])
+    # print(merged_arr_2)  
+
+  return merged_arr_2        
+  
+print(mergeSortedArrays([0,3,4,31], [3,4,5,30]))
+# print(mergeSortedArrays([0,3,4,31], [2,4,6,30]))
