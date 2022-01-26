4a2df3441dd1cec30ecbbaeef55565a25fe7c7ce
Aditya Singh <adips@amazon.com>
2022-01-26 23:41:44 +0530

    fix

--- practice/mergedSortedArrays.py
+++ practice/mergedSortedArrays.py
@@ -1,6 +1,7 @@
 def mergeSortedArrays(arr1, arr2):
   merged_arr_1=[]
   # Way 1
+  # To be done
   for i in range(len(arr1)):
     for j in range(len(arr2)):
       if arr1[i] < arr2[j]:
@@ -10,6 +11,7 @@ def mergeSortedArrays(arr1, arr2):
       else:
         merged_arr_1.append(arr2[j])
         # arr2.remove(arr2[j])  
+        
   # Way 2
   # Works with same length arrays
   #  Iterate simantaneously and compare elems from both arrays whichever is less goes first. Also if an item already exists skip that.
