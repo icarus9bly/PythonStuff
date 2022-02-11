3c1a0d2b577c9267922ad09266d78345ce77021b
Aditya Singh <adips@amazon.com>
2022-02-11 19:00:44 +0530

    Adding documentation

--- recursion.py
+++ recursion.py
@@ -1,9 +1,10 @@
 def factorial(n):
+    """Calculate the factorial of 'n' number."""
     # Base case: Condition when this recursive function should stop execution. We know 1!=1 so that's our base case
     if n == 1:
         return 1
     return n*factorial(n-1)
-
+ 
 print(factorial(10))
 
 # 8!= 8*7*6*5*4*3*2*1 = 8*7!
@@ -16,3 +17,16 @@ print(factorial(10))
 # 3*2!
 # 2*1!=2
 # 1!=1
+
+def fibonacci(n):
+    """Calculate nth number in the fibonacci sequence."""
+    # Base case:
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1)+fibonacci(n-2)
+
+
+print(fibonacci(4))
+0, 1, 1 , 2, 3, 5
