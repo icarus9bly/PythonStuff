fdd3c7e95396b1da86d26c348e1127deb596c33c
Aditya Singh <adips@amazon.com>
2022-04-16 14:07:09 +0530

    added bubble sort

--- BubbleSort.py
+++ BubbleSort.py
@@ -6,11 +6,13 @@ def bubble_sort(x):
                 # x[j] = x[j+1]
                 # x[j+1] = temp
                 x[j], x[j+1] = x[j+1], x[j] # More pythonic
+            print(x)
     return x
 
 
 def main():
     list1 = [45,5,11,6,34,54543,121,2,1]
+    print(list1)
     print(bubble_sort(list1))
 
 if __name__ == "__main__":
