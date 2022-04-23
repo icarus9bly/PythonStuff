52da3bfbf5e53adfbc725501d2b413ab3788b561
Aditya Singh <adips@amazon.com>
2022-04-23 16:09:37 +0530

    added selection sort

--- SelectionSort.py
+++ SelectionSort.py
@@ -11,20 +11,19 @@ def selection_sort(x):
         x[smallest] = temp
     return x
 
-# def selection_sort_approach2(x):
-    """To do"""
-#     for i in range(len(x)):
-#         smallest = i
-#         for j in range(len(x)):
-#             if x[smallest] > x[j]:
-#                 smallest = j
-#         x[i], x[smallest] = x[smallest], x[i]
-#     return x
+def selection_sort_approach2(x):
+    for i in range(len(x)):
+        smallest = i
+        for j in range(i+1,len(x)):
+            if x[smallest] > x[j]:
+                smallest = j
+        x[i], x[smallest] = x[smallest], x[i]
+    return x
 
 def main():
     list1 = [45,5,11,6,34,54543,121,2,1]
     print(selection_sort(list1))
-    # print(selection_sort_approach2(list1))
+    print(selection_sort_approach2(list1))
 
 if __name__ == "__main__":
     main()
